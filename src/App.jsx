0234275f99373a4d677e2fda37339bced025ec93
Versha <vershadugg68@gmail.com>
2024-10-21 23:21:19 +0530

    Your descriptive message about the changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { Counter } from './Usestate/Counter'
 import { ThemeToggler } from './Usestate/Theme'
 
 
 function App() {
  return (
-  <ThemeToggler/>
+  <Counter/>
   )
 }
 
--- src/Usestate/Counter.jsx
+++ src/Usestate/Counter.jsx
@@ -1,22 +1,22 @@
-//Create a counter component that increments and decrements a count. Use the useState hook to manage the count value.
-// Include buttons for incrementing, decrementing, and resetting the count to zero.
-// import { useState } from "react";
-// export const Counter=()=>{
-//     const[count,setCount]=useState(0);
-//     const incre=()=>{
-//         setCount(previous=>previous+1);
-//     }
-//     const decre=()=>{
-//         setCount(previous=>previous-1);
-//     }
-//     return(
-//         <div>
-//             <div>
-//                 <h1>{count}</h1>
-//             </div>
-//     <button type="button" className="btn-one" onClick={incre}>Increment</button>
-//     <button type="button" className="reset" onClick={()=>setCount(0)}>Reset</button>
-//     <button type="button" className="btn-two" onClick={decre}>Decrement</button>
-//         </div>
-//     )
-// }
+// Create a simple counter component that displays a number. Include buttons to increment and decrement the counter value.
+// Requirements:
+// •	Initialize the counter state to 0.
+// •	Create two buttons: "Increment" and "Decrement".
+// •	Update the counter when the buttons are clicked.
+import { useState } from "react"
+export const Counter=()=>{
+    const[value,setValue]=useState(0);
+    const increment=()=>{
+        setValue(value+1);
+    }
+    const decrement=()=>{
+        setValue(value-1)
+    }
+    return(
+        <div>
+            <h1>Your Counter value id:- {value}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
